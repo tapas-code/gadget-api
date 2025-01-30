e5d0d62915814c7ecd984a7757c5b7f37b9a749e
tapas-code <tapas.code247@gmail.com>
2025-01-30 16:06:29 +0530

    Modify Redis config

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -3,14 +3,15 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-if (!process.env.REDIS_URL) {
-  throw new Error("REDIS_URL is not defined.");
-}
+console.log("🚀 Redis URL:", process.env.REDIS_URL);
 
 const redis = new Redis(process.env.REDIS_URL as string, {
   tls: {
-    rejectUnauthorized: false,
+    rejectUnauthorized: false, 
   },
 });
+
+redis.on("connect", () => console.log("✅ Connected to Redis!"));
+redis.on("error", (err) => console.error("❌ Redis Error:", err));
 
 export default redis;
